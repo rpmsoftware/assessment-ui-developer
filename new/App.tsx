3d7b555ad0552fa3579c199243f8b23592033626
leeanderson410 <anderson.lee.m@gmail.com>
2022-06-26 18:39:11 -0400

    Implement App.tsx
    Implement new react component that talks to old mootools component.

--- new/App.tsx
+++ new/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-    // Return output to be rendered by index.tsx
+    const [divContent, setDivContent] = useState("Click a button");
+    let oldButtons: NodeList;
+
+    const legacyHandler = () => {
+        console.log("legacyHandler");
+        if (!oldButtons) {
+            // Add click event to legacy mootools buttons
+            oldButtons = document.querySelectorAll("#old > button");
+            oldButtons.forEach((oldButton: HTMLButtonElement) => {
+                console.log("Add click event to button " + oldButton.innerHTML.toString());
+                oldButton.addEventListener("click", () => buttonClick(oldButton));
+            });
+        }
+    };
+
+    const buttonClick = (button: HTMLButtonElement) => {
+        console.log("Button " + button.innerHTML.toString() + " clicked");
+        setDivContent("Button " + button.innerHTML.toString() + " clicked");
+    };
+
+    useEffect(() => {
+        legacyHandler();
+    }, []);
+
+    // Return app output to be rendered by index.tsx
     return (
         <div>
-            <h1>Hello from React!</h1>
+            {divContent}
         </div>
     );
 }
